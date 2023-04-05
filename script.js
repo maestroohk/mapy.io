e55216eea582fbfd8b9818258c91e6252b4ac65e
maestroohk <henryk51294@gmail.com>
2023-04-05 21:08:42 +0300

    changes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ if (navigator.geolocation) {
 
       let map = L.map('map').setView(coords, 10);
 
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
