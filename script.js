9fea646af2ff9feb3492c8294ce29d20cc4783ac
maestroohk <henryk51294@gmail.com>
2023-04-05 11:56:04 +0300

    added map

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ if (navigator.geolocation) {
       const { latitude, longitude } = position.coords;
       console.log(`${latitude}, ${longitude}`);
       console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      let map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => {
       console.log("Couldn't get your location");
