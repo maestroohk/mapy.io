a925043e54e561e8f2e0ed365930680584546805
maestroohk <henryk51294@gmail.com>
2023-04-06 07:40:38 +0300

    zoom

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      let map = L.map('map').setView(coords, 10);
+      let map = L.map('map').setView(coords, 15);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
